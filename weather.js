fee50c2976988158e070a0a45d4b1833a2f57531
RossEagar <60120360+RossEagar@users.noreply.github.com>
2020-01-20 19:01:03 -0600

    Create weather.js

---
+++ weather.js
@@ -0,0 +1,54 @@
+const https = require('https')
+
+process.argv.slice(2).forEach(function (val, index, array) {
+  GetWeather(val)
+  GetTime(val)
+})
+
+function GetWeather(location) {
+
+                https.get('https://api.openweathermap.org/data/2.5/weather?q=' + location + '&APPID=fa393bde67f7c0529ed93a7e8699b3d2', (resp) => {
+                  let data = ''
+
+                  resp.on('data', (chunk) => {
+                    data += chunk
+                  });
+
+                  resp.on('end', () => {
+                                GetWeatherCallback(location, data)
+                  });
+
+                }).on("error", (err) => {
+                  console.log("Error: " + err.message)
+                })
+                
+}
+
+function GetTime(location) {
+
+                https.get('https://www.amdoren.com/api/timezone.php?api_key=3gVhM7DHnUKWFmSeF5bSKfpuWHwnKe&loc=' + location, (resp) => {
+                 let data = ''
+
+                  resp.on('data', (chunk) => {
+                    data += chunk
+                  });
+
+                  resp.on('end', () => {
+                                GetTimeCallback(location, data)
+                  });
+
+                }).on("error", (err) => {
+                  console.log("Error: " + err.message)
+                })
+                
+}
+
+function GetWeatherCallback(location,data) {
+                let weatherInfo = JSON.parse(data)
+  console.log(location + ' Local Weather: ' + weatherInfo.description + ' - ' + weatherInfo.temp)
+}
+
+function GetTimeCallback(location,data) {
+                let timeInfo = JSON.parse(data)
+  console.log(location + ' Local Time: ' + timeInfo.time)
+}
